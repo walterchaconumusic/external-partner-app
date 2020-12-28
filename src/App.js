9f4e383bdebb4011e6ceeae704fe7e02c4a63cc8
Walter Chacon <walter.chacon.umusic@gmail.com>
2020-12-28 22:29:58 +0100

    Fixing iframe title property missing error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>External App</h1>
-        <iframe width="1400" height="800" src='https://builder.io/login/oidc/mira-walter'></iframe>
+        <iframe title="external-app" width="1400" height="800" src='https://builder.io/login/oidc/mira-walter'></iframe>
       </header>
     </div>
   );
